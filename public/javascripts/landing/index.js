32b16759f5a569a7e8cbf1659b99f0bc25fead8f
shonihei <shonihei@gmail.com>
2018-03-19 13:52:53 -0400

    added performant scroll event listener

---
+++ public/javascripts/landing/index.js
@@ -0,0 +1,31 @@
+window.onload = () => {
+    const navbar = document.getElementById('navbar')
+    const mouse = document.getElementById('scroll-down-yo')
+    const navbarBreakPoint = 150
+    let lastScrollY = 0
+    let ticking = false
+    const update = () => {
+        if (lastScrollY >= navbarBreakPoint) {
+            navbar.classList.remove('transparent')
+            mouse.classList.add('hidden')
+        } else {
+            navbar.classList.add('transparent')
+            mouse.classList.remove('hidden')
+        }
+        ticking = false
+    }
+
+    const requestTick = () => {
+        if (!ticking) {
+            window.requestAnimationFrame(update);
+            ticking = true
+        }
+    }
+
+    const onScroll = () => {
+        lastScrollY = window.scrollY
+        requestTick()
+    }
+
+    window.addEventListener('scroll', onScroll)
+}
