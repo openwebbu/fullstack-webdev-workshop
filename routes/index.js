16292905d507495cdcfd94d1fd31f93829af28b5
shonihei <shonihei@gmail.com>
2018-03-21 13:30:56 -0400

    started working on a function to dynamically import routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router()
+const fs = require('fs')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' })
+    res.render('index', { title: 'Home' })
+})
+
+fs.readdirSync(`${__dirname}/api`).forEach(function(route) {
+    [route, suffix] = route.split('.')
+    if (suffix !== "js" || route === "index") return;
+    console.log(`Loading route: /api/${route}`);
+    router.use(`/api/${route}`, require(`./api/${route}`));
 })
 
 module.exports = router
--- routes/places.js
+++ routes/places.js
@@ -2,41 +2,18 @@ const express = require('express')
 const router = express.Router()
 const Place = require('../models/place')
 
-router.get('/:id', function(req, res, next) {
-    res.render('places/place', { title: 'Express' })
-})
-
-router.post('/', function(req, res, next) {
-    const {name, description, address, phone, website} = req.body
-    if (!name) {
-        return res.status(400).json({
-            success: false,
-            message: 'name missing'
-        })
-    }
-    const newPlace = new Place({
-        name: name,
-        description: description || '',
-        address: address || '',
-        phone: phone || '',
-        website: website || '',
-    })
-    newPlace.save(function(err, user) {
+// READ
+router.get('/:slug', function(req, res, next) {
+    const slug = req.params.slug
+    Place.findOne({ slug: slug }, function(err, place) {
         if (err) {
-            if (err.code === 11000) {
-                return res.status(400).json({
-                    success: false,
-                    err: 'place with the given parameters already exit'
-                })   
-            }
-            console.log(err)
-            return res.status(400).json({
-                success: false,
-            })
+            res.render('error')
         }
-        return res.status(200).json({
-            success: true,
-        })
+        
+        if (!place) {
+            res.render('places/search')
+        }
+        res.render('places/place', { place: place, title: place.name })
     })
 })
 
