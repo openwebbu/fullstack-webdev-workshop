a93aa96173278dad1cc872a648bd278ddf491bc9
shonihei <shonihei@gmail.com>
2018-03-21 23:32:42 -0400

    cleaned up frontend transition on signin page

--- public/javascripts/accounts/signup/index.js
+++ public/javascripts/accounts/signup/index.js
@@ -1,16 +1,40 @@
 window.onload = () => {
-    const box = document.getElementById('box')
+    let signinMode = false
 
+    const box = document.getElementById('box')
     const signin = document.getElementById('signin')
     const signup = document.getElementById('signup')
-    signin.addEventListener('click', () => {
-        box.classList.add('signin')
-        signin.classList.add('selected')
-        signup.classList.remove('selected')
-    })
-    signup.addEventListener('click', () => {
-        box.classList.remove('signin')
-        signin.classList.remove('selected')
-        signup.classList.add('selected')
-    })
+    const submitBtn = document.getElementById('submit-btn')
+    const fields = document.getElementsByClassName('text-field')
+
+    signin.addEventListener('click', toggleSignIn)
+    signup.addEventListener('click', toggleSignIn)
+
+    function toggleSignIn() {
+        signinMode = !signinMode
+        if (signinMode) {
+            box.classList.add('signin')
+            signin.classList.add('selected')
+            signup.classList.remove('selected')
+        } 
+        else {
+            box.classList.remove('signin')
+            signin.classList.remove('selected')
+            signup.classList.add('selected')
+            
+        }
+        setTimeout(function() {
+            if (signinMode) {
+                submitBtn.innerText = 'Sign in'
+            }
+            else {
+                submitBtn.innerText = 'Create an account'
+            }
+        }, 300)
+        setTimeout(function() {
+            for (let field of fields) {
+                field.value = ''
+            }
+        }, 700)
+    }
 }
