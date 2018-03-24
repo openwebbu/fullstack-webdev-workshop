086d8606de7dde0cdbe51043f7963fedad9e65d8
shonihei <shonihei@gmail.com>
2018-03-24 16:16:22 -0400

    added natural language processing

--- models/review.js
+++ models/review.js
@@ -1,19 +1,67 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const language = require('@google-cloud/language')
+const timeAgo = require('time-ago')
+
+const Schema = mongoose.Schema
 const ObjectId = Schema.Types.ObjectId
+const client = new language.LanguageServiceClient({
+    projectId: process.env.GCLOUD_PROJECT_ID,
+    keyFilename: process.env.GLOUD_KEYFILE_PATH,
+})
 
 const Review = new Schema({
-    owner: {
+    by: {
         type: ObjectId,
         ref: 'User',
         required: true,
     },
-    text: String,
+    text: {
+        type: String,
+        required: true,
+    },
     place: {
         type: ObjectId,
         ref: 'Place',
         required: true,
     },
+    sentimentScore: {
+        type: Number,
+    },
+    sentimentMag: {
+        type: Number,
+    },
+    created: {
+        type: Date,
+        default: Date.now,
+    }
+})
+
+Review.methods.ago = function() {
+    return timeAgo.ago(this.created)
+}
+
+Review.pre('save', function(next) {
+    const review = this
+
+    if (!review.isModified('text')) next()
+
+    const document = {
+        content: review.text,
+        type: 'PLAIN_TEXT',
+    }
+
+    client
+        .analyzeSentiment({document: document})
+        .then(results => {
+            const sentiment = results[0].documentSentiment;
+
+            review.sentimentScore = sentiment.score
+            review.sentimentMag = sentiment.magnitude
+            next()
+        })
+        .catch(err => {
+            next(err)
+        })
 })
 
 module.exports = mongoose.model('Review', Review)
--- routes/places.js
+++ routes/places.js
@@ -1,22 +1,32 @@
 const express = require('express')
 const router = express.Router()
 const Place = require('../models/place')
+const authMiddleware = require('../middleware/auth')
+const Review = require('../models/review')
 
 router.get('/:slug', function(req, res, next) {
     const slug = req.params.slug
-    Place.findOne({ slug: slug }, function(err, place) {
-        if (err) {
-            res.render('error')
-        }
-        
-        if (!place) {
-            res.render('places/search')
-        }
-        res.render('places/place', { place: place, title: place.name })
-    })
+    Place
+        .findOne({ slug: slug })
+        .populate({
+            path:     'reviews',			
+            populate: { path:  'by',
+                        model: 'User' }
+        })
+        .exec(function(err, place) {
+
+            if (err) {
+                res.render('error')
+            }
+            
+            if (!place) {
+                res.render('places/search')
+            }
+            res.render('places/place', { place: place, title: place.name })
+        })
 })
 
-router.get('/:slug/new-review', function(req, res, next) {
+router.get('/:slug/new-review', authMiddleware.isAuthenticated, function(req, res) {
     const slug = req.params.slug
     Place.findOne({ slug: slug }, function(err, place) {
         if (err) {
@@ -27,6 +37,47 @@ router.get('/:slug/new-review', function(req, res, next) {
             res.render('places/search')
         }
         res.render('places/new-review', { place: place, title: place.name })
+    })
+})
+
+router.post('/:slug/new-review', function(req, res) {
+    const slug = req.params.slug
+    const user = req.session.user
+    const {text} = req.body
+
+    Place.findOne({ slug: slug }, function(err, place) {
+        if (err) {
+            res.render('error')
+        }
+        
+        const newReview = new Review({
+            by: user,
+            text: text,
+            place: place,
+        })
+
+        newReview.save(function(err, review) {
+            if (err) {
+                console.log(err)
+                return res.status(400).json({
+                    success: false,
+                })
+            }
+            place.reviews.push(review)
+            let scoreSum = place.sentimentScoreAvg * place.numReviews + newReview.sentimentScore
+            let magSum = place.sentimentMagAvg * place.numReviews + newReview.sentimentMag
+            place.numReviews += 1
+            place.sentimentScoreAvg = scoreSum / place.numReviews
+            place.sentimentMagAvg = magSum / place.numReviews
+
+            place.save(function(err, place) {
+                if (err) {
+                    return res.send('oh no')
+                }
+                return res.redirect(`/places/${slug}`)
+            })
+            
+        })
     })
 })
 
